739d98cdf31d534754694ae1925da09e843d88ad
Thomas Bille <thomas.bille@canonical.com>
2019-04-09 10:44:45 +0100

    Don't add prefix if none

--- canonicalwebteam/docs/blueprint.py
+++ canonicalwebteam/docs/blueprint.py
@@ -89,7 +89,10 @@ def build_blueprint(url_prefix, discourse_url, frontpage_id, category_id):
 
         frontpage, nav_html = discourse.parse_frontpage()
 
-        return flask.redirect(url_prefix + frontpage["path"])
+        if url_prefix is not "/":
+            return flask.redirect(url_prefix + frontpage["path"])
+        else:
+            return flask.redirect(frontpage["path"])
 
     @discourse_blueprint.route("/<path:path>")
     def document(path):
