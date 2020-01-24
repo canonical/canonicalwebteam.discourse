2a102488cba653d52ffdf60c4c33bd4d4e6ce7f0
Thomas Bille <toto@canonical.com>
2020-01-24 15:56:49 +0000

    Fix slug issue wit non alpha num char

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.discourse_docs",
-    version="0.10.1",
+    version="0.10.2",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-webteam/canonicalwebteam.docs",
