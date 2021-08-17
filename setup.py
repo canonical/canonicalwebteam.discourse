38ceff3be6f035ffd85ddcd42d426a63c6cbb98b
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2021-08-17 16:58:11 +0100

    Hidden nav items

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.discourse",
-    version="4.0.3",
+    version="4.0.4",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-webteam/canonicalwebteam.docs",
