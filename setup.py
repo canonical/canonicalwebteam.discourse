9ac19e7c9ca0045d9b252532778322376bcb767c
petesfrench <peterfrench94@gmail.com>
2023-03-06 11:40:59 +0100

    Build parse_docs_metadata function

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.discourse",
-    version="5.0.4",
+    version="5.2.4",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url=(
