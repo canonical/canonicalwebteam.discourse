55bdd75989aaeda35c9a458800d54057f8a59d64
Lukewh <luke@lukewh.com>
2023-03-17 15:16:10 +0000

    Update README and bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.discourse",
-    version="5.0.4",
+    version="5.0.5",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url=(
