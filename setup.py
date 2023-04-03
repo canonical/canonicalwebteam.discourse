c5f455e3ce017ee127aba9de1b86d30763ebedb3
Adam Collard <adam.collard@canonical.com>
2023-04-03 09:05:59 +0100

    Bumpity-le-bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.discourse",
-    version="5.3.0",
+    version="5.4.0",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical/canonicalwebteam.discourse",
