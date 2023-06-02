b8180b8254c0e4d2c102ff5c521c3af1d49fc031
Robin Winslow Morris <robin@robinwinslow.co.uk>
2023-06-02 12:32:15 +0100

    Fix indefinite caching of index content by undoing "ensure_parsed" work
    This undoes the work done in [PR 156][1], commits [2fc9e17][2], [b7d859][3] and [965aa6][4].
    
    That work was attempting to avoid parsing through the index topic content more often than necessary,
    by simply skipping the fetching of that content entirely once it's been downloaded once.
    
    This caused big problems[5] because no changes to any of the documentation index content, including
    navigation updates, would be reflected until the application is re-released.
    
    There may be more sensible ways to avoid parsing the index topic content too many times,
    but for now this work needs to be rolled back.
    
    Fixes https://github.com/canonical/canonicalwebteam.discourse/issues/163
    
    [1]: https://github.com/canonical/canonicalwebteam.discourse/pull/156
    [2]: https://github.com/canonical/canonicalwebteam.discourse/pull/156/commits/2fc9e173404c93b19cbe22c7d1e6b002000b47cb
    [3]: https://github.com/canonical/canonicalwebteam.discourse/pull/156/commits/b7d859f1d9fb7b93bb46333ce4396987328d2af4
    [4]: https://github.com/canonical/canonicalwebteam.discourse/pull/156/commits/965aa68b87ba7c748d2835f86b9a3b99772e2e5d
    [5]: https://github.com/canonical/canonicalwebteam.discourse/issues/163

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,6 @@
 import json
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 import warnings
 
 from bs4 import BeautifulSoup
@@ -10,7 +10,6 @@ import requests
 from canonicalwebteam.discourse.models import DiscourseAPI
 from canonicalwebteam.discourse.parsers.base_parser import BaseParser
 from canonicalwebteam.discourse.parsers.docs import DocParser
-from canonicalwebteam.discourse.parsers.tutorials import TutorialParser
 
 EXAMPLE_CONTENT = """
 <p>Some homepage content</p>
@@ -135,50 +134,6 @@ class TestBaseParser(unittest.TestCase):
         )
 
         self.assertEqual("/t/sample--text/1", parsed_topic["topic_path"])
-
-
-class TestDocParserEnsureParsed(unittest.TestCase):
-    def test_ensure_parsed(self):
-        """Ensure parsed will call parse if and only if index_topic is None."""
-        discourse_api = DiscourseAPI("https://base.url", session=MagicMock())
-
-        parser = DocParser(
-            api=discourse_api,
-            index_topic_id=1,
-            url_prefix="/",
-        )
-        with patch.object(parser, "parse", autospec=True) as mock_parse:
-            self.assertIsNone(parser.index_topic)
-            parsed_already_first = parser.ensure_parsed()
-            self.assertFalse(parsed_already_first)
-            mock_parse.assert_called_once_with()
-            mock_parse.reset_mock()
-            parser.index_topic = object()
-            parsed_already_second = parser.ensure_parsed()
-            self.assertTrue(parsed_already_second)
-            mock_parse.assert_not_called()
-
-
-class TestTutorialParser(unittest.TestCase):
-    def test_ensure_parsed(self):
-        """Ensure parsed will call parse if and only if index_topic is None."""
-        discourse_api = DiscourseAPI("https://base.url", session=MagicMock())
-
-        parser = TutorialParser(
-            api=discourse_api,
-            index_topic_id=1,
-            url_prefix="/",
-        )
-        with patch.object(parser, "parse", autospec=True) as mock_parse:
-            self.assertIsNone(parser.index_topic)
-            parsed_already_first = parser.ensure_parsed()
-            self.assertFalse(parsed_already_first)
-            mock_parse.assert_called_once_with()
-            mock_parse.reset_mock()
-            parser.index_topic = object()
-            parsed_already_second = parser.ensure_parsed()
-            self.assertTrue(parsed_already_second)
-            mock_parse.assert_not_called()
 
 
 class TestDocParser(unittest.TestCase):
