ace8fdfc30479044529a894a42177629ee059f12
LukeWH <luke@lukewh.com>
2023-11-22 11:30:44 +0000

    Fix srcset lookup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.discourse",
-    version="5.4.6",
+    version="5.4.7",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical/canonicalwebteam.discourse",
